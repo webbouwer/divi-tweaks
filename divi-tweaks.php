12755c4a82b999075c6268ec8b55d49090f112f3
Webbouwer <support@webdesigndenhaag.net>
2020-12-17 22:56:29 +0100

    Create divi-tweaks.php

---
+++ divi-tweaks.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Divi Tweaks
+Plugin URI: https://webbouwer.org
+Description: Some tweaks for Divi Theme
+Version:     0.5.6
+Author:      Webbouwer
+Author URI:  http://webbouwer.org
+Text Domain: divi-tweaks
+License:     © Webbouwer All rights reserved
+License URI: https://webbouwer.org
+Github Plugin URI: https://github.com/webbouwer/divi-tweaks
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
